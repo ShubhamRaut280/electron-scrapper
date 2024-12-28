29c839345a54088615db7c8fcd6eabbfdbfdf741
Shubham Raut <124999374+ShubhamRaut280@users.noreply.github.com>
2024-12-28 15:52:22 +0530

    tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
-import './App.css'
+import './styles/tailwind.css';
+
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className="App">
+      
+    </div>
   )
 }
 
