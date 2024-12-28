266aca8fa3245a68e1c79bb13f965fcd04f67997
Shubham Raut <124999374+ShubhamRaut280@users.noreply.github.com>
2024-12-28 17:33:12 +0530

    setup navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ function Header({ handleDownload }) {
   return (
     <div className="flex justify-end items-center bg-[#ff9950] h-[60px] w-screen">
       <div
-        className="flex justify-center items-center h-full w-[200px] cursor-pointer transition-all duration-300 ease-in-out transform hover:scale-105 hover:bg-[#e78f3d] active:bg-[#d07a2a] focus:outline-none focus:ring-2 focus:ring-[#ff9950] focus:ring-offset-2"
+        className="flex justify-center items-center h-full w-[200px] cursor-pointer transition-all duration-300 ease-in-out transform  hover:bg-[#e78f3d] active:bg-[#d07a2a] focus:outline-none focus:ring-2 focus:ring-[#ff9950] focus:ring-offset-2"
         onClick={handleDownload}
       >
         <FiDownload className="h-8 w-auto text-white" />
--- src/components/Pages/Setup.jsx
+++ src/components/Pages/Setup.jsx
@@ -1,15 +1,26 @@
 // src/pages/AboutPage.js
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Setup() {
+  const navigate = useNavigate();
+
+  const handleDownload = () => {  navigate('/'); };
+
   return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div className="bg-green-200 p-8 rounded-lg shadow-lg text-center">
-        <h2 className="text-3xl font-bold mb-4">About Page</h2>
-        <p className="mb-4">This is the About Page of the app.</p>
-        <Link to="/" className="text-blue-600 hover:underline">Back to Home Page</Link>
+    <div className="flex justify-start items-center bg-[#ff9950] h-[60px] w-screen">
+      <div
+        className="flex justify-center items-center h-full w-[100px] cursor-pointer transition-all duration-300 ease-in-out transform  hover:bg-[#e78f3d] active:bg-[#d07a2a] focus:outline-none focus:ring-2 focus:ring-[#ff9950] focus:ring-offset-2"
+        onClick={handleDownload}
+      >
+        <FiArrowLeft className="h-8 w-auto text-white" />
+
       </div>
+      <span className='font-sans-serif font-semibold ms-5'>Complete the setup</span>
+
+      
+
     </div>
   );
 }
